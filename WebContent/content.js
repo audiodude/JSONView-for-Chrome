b9f15e8b656fe3516b510837626da648f24ca77b
Travis Briggs <audiodude@gmail.com>
2014-02-21 18:07:58 -0800

    Add small button for downloading JSON contents as a CSV file.
    Unfortunately this pulls in all of JQuery and all of the JQuery-CSV file. But it works!

--- WebContent/content.js
+++ WebContent/content.js
@@ -54,6 +54,8 @@ function displayUI(theme, html) {
 	document.body.appendChild(statusElement);
 	toolboxElement = document.createElement("div");
 	toolboxElement.className = "toolbox";
+  toCsvElement = document.createElement("a");
+  toCsvElement.innerText = "csv";
 	expandElement = document.createElement("span");
 	expandElement.title = "expand all";
 	expandElement.innerText = "+";
@@ -67,6 +69,7 @@ function displayUI(theme, html) {
 	optionsElement = document.createElement("img");
 	optionsElement.title = "options";
 	optionsElement.src = chrome.runtime.getURL("options.png");
+  toolboxElement.appendChild(toCsvElement);
 	toolboxElement.appendChild(expandElement);
 	toolboxElement.appendChild(reduceElement);
 	toolboxElement.appendChild(viewSourceElement);
@@ -76,6 +79,7 @@ function displayUI(theme, html) {
 	document.body.addEventListener('mouseover', onmouseMove, false);
 	document.body.addEventListener('click', onmouseClick, false);
 	document.body.addEventListener('contextmenu', onContextMenu, false);
+  toCsvElement.addEventListener('click', showCsv, false);
 	expandElement.addEventListener('click', onexpand, false);
 	reduceElement.addEventListener('click', onreduce, false);
 	optionsElement.addEventListener("click", function() {
@@ -148,6 +152,80 @@ function processData(data) {
 			jsonText = data.text;
 			formatToHTML(data.fnName, data.offset);
 		}
+}
+
+// adapted from csvkit's recursive JSON flattening mechanism:
+// https://github.com/onyxfish/csvkit/blob/master/csvkit/convert/js.py#L8-L27
+function parse_object(obj, path) {
+    if (path == undefined)
+        path = "";
+
+    var type = $.type(obj);
+    var scalar = (type == "number" || type == "string" || type == "boolean" || type == "null");
+
+    if (type == "array" || type == "object") {
+        var d = {};
+        for (var i in obj) {
+
+            var newD = parse_object(obj[i], path + i + "/");
+            $.extend(d, newD);
+        }
+
+        return d;
+    }
+
+    else if (scalar) {
+        var d = {};
+        var endPath = path.substr(0, path.length-1);
+        d[endPath] = obj;
+        return d;
+    }
+
+    // ?
+    else return {};
+}
+
+
+// otherwise, just find the first one
+function arrayFrom(json, key) {
+    if ($.type(json) == "array")
+        return json;
+    else if (key)
+        return json[key];
+
+    else {
+        for (var key in json) {
+            if ($.type(json[key]) == "array")
+                return json[key];
+        }
+
+        // none found
+        return [];
+    }
+}
+
+// 1) find the primary array to iterate over
+// 2) for each item in that array, recursively flatten it into a tabular object
+// 3) turn that tabular object into a CSV row using jquery-csv
+function json2csv(json) {
+    var inArray = arrayFrom(json);
+
+    var outArray = [];
+    for (var row in inArray)
+        outArray[outArray.length] = parse_object(inArray[row]);
+
+    return $.csv.fromObjects(outArray);
+}
+
+function showCsv(event) {
+  var csv = json2csv(jsonObject);
+
+  // now, make a data: URI out of it
+  // thanks to http://jsfiddle.net/terryyounghk/KPEGU/
+  // and http://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
+  var uri = "data:text/csv;charset=utf-8," + encodeURIComponent(csv);
+
+  this.href = uri;
 }
 
 function ontoggle(event) {
